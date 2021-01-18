ab37a891951d489fd22e12ad2f15fa1f753c2541
Clipsham, Michael <Michael.Clipsham@det.nsw.edu.au>
2021-01-18 11:02:51 +1100

    Environment variables updated per deployment step 3

--- Source/QuestionsTabApp/src/environments/environment.prod.ts
+++ Source/QuestionsTabApp/src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
     production: false,
-    apiBaseUrl: "https://qbotdemotesting.azurewebsites.net/api/Request/",
-    selfUrl: "https://qbotdemotesting-questions.azurewebsites.net",
+    apiBaseUrl: "https://schoolsnswuatqbot.azurewebsites.net/api/Request/",
+    selfUrl: "https://schoolsnswuatqbot-questions.azurewebsites.net",
 
     authConfig: {
         instance: "https://login.microsoftonline.com/",
-        tenantId: "7520da05-6887-4d9d-9876-45bbf886ac78",
-        clientId: "62a0eea7-3c79-450e-8083-ab31364d45a3",
+        tenantId: "c6198395-22e6-4778-9809-ac8dcfd76901",
+        clientId: "e140df66-1580-4a99-bf40-b1a5687aab85",
         redirectUri: "/app-silent-end",
         cacheLocation: "localStorage",
         navigateToLoginRequestUrl: false,
